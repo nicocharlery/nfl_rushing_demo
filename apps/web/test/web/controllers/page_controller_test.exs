7595b0afda282b5abc04e5f5876d21d5501a1a63
Nicolas Charlery <nicolas@bokay.io>
2020-01-26 16:28:50 -0500

    refactor: Clean initial page layout

--- apps/web/test/web/controllers/page_controller_test.exs
+++ apps/web/test/web/controllers/page_controller_test.exs
@@ -3,6 +3,7 @@ defmodule Web.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+
+    assert html_response(conn, 200) =~ "NFL Rushing"
   end
 end
