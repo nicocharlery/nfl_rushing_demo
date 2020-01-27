dfa116ef9198706217914372ed1544b3ca38719b
Nicolas Charlery <nicolas@bokay.io>
2020-01-26 20:51:33 -0500

    chore: Install React

---
+++ apps/web/assets/js/table.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+        <div>React is loaded!</div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
--- apps/web/assets/webpack.config.js
+++ apps/web/assets/webpack.config.js
@@ -22,10 +22,13 @@ module.exports = (env, options) => ({
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+	  options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
         }
       },
       {
