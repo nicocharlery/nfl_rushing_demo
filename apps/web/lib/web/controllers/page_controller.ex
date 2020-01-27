40206529d47f41c76e3574de1088d0c33735e1ca
Nicolas Charlery <nicolas@bokay.io>
2020-01-27 18:59:27 -0500

    refactor: Remove traces of previous implementation

--- apps/web/lib/web/controllers/page_controller.ex
+++ apps/web/lib/web/controllers/page_controller.ex
@@ -2,14 +2,6 @@ defmodule Web.PageController do
   use Web, :controller
 
   def index(conn, _params) do
-    render(
-      conn,
-      "index.html",
-      players: player_repository(conn).all
-    )
-  end
-
-  defp player_repository(conn) do
-    Map.get(conn.private, :player_repository, Nfl.Players.PlayerRepository)
+    render(conn, "index.html")
   end
 end
